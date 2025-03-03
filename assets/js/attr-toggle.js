5b2766dd0af4c579b5097299f57879058a392c90
Vladislav Savitski <vladislav.savitski.prog@gmail.com>
2025-03-03 13:21:02 +0300

    fix toggle

--- assets/js/attr-toggle.js
+++ assets/js/attr-toggle.js
@@ -1,29 +1,30 @@
 jQuery(document).ready(function($) {
-    function updateVariation(selectedSize) {
-        let productId = selectedSize === "30cm" ? "134" : "135";
+    function updateVariation(selectedSize, container) {
+        let productId = selectedSize === "30cm" ? container.find(".pizza__weight_val").first().attr("class").match(/product-(\d+)/)[1]
+            : container.find(".pizza__weight_val").last().attr("class").match(/product-(\d+)/)[1];
 
-        // Убираем active у всех переключателей размеров
-        $(".size-toggle").removeClass("active");
-        $(".pizza__weight_val, .pizza__price").removeClass("active");
+        container.find(".size-toggle").removeClass("active");
+        container.find(".pizza__weight_val, .pizza__price").removeClass("active");
 
-        // Добавляем active выбранным элементам
-        $(".size-toggle input[value='" + selectedSize + "']").parent().addClass("active");
-        $(".pizza__weight_val.product-" + productId).addClass("active");
-        $(".pizza__price.product-" + productId).addClass("active");
+        container.find(".size-toggle input[value='" + selectedSize + "']").parent().addClass("active");
+        container.find(".pizza__weight_val.product-" + productId).addClass("active");
+        container.find(".pizza__price.product-" + productId).addClass("active");
 
-        // Обновляем скрытые поля вариации
-        $(".variation_id").val(productId);
-        $("select[name='attribute_pa_size']").val(selectedSize).trigger("change");
+        container.find(".variation_id").val(productId);
+        container.find("select[name='attribute_pa_size']").val(selectedSize).trigger("change");
     }
 
     $(".toggle-container .size-toggle").on("click", function() {
+        let container = $(this).closest(".pizza__item");
         let selectedSize = $(this).find("input").val();
-        updateVariation(selectedSize);
+        updateVariation(selectedSize, container);
     });
 
-    // Устанавливаем начальное значение вариации при загрузке
-    if ($('.woocommerce-variation-add-to-cart').length) {
-        let initialSize = $("select[name='attribute_pa_size']").val() || "30cm"; // Если не выбрано, ставим 30cm
-        updateVariation(initialSize);
-    }
+    $(".pizza__item").each(function() {
+        let container = $(this);
+        if (container.find('.woocommerce-variation-add-to-cart').length) {
+            let initialSize = container.find("select[name='attribute_pa_size']").val() || "30cm"; // Если не выбрано, ставим 30cm
+            updateVariation(initialSize, container);
+        }
+    });
 });
