91478aed8f6f1ba47c4cf54c9877bef3c6e87343
Vika3534543 <mariamovna@icloud.com>
2025-03-12 19:07:30 +0300

    add styles for mobile

--- header.php
+++ header.php
@@ -19,6 +19,27 @@
             <li><a href="<?php echo esc_url(home_url('/')); ?>">Главная</a></li>
             <li><a href="<?php echo esc_url(home_url('/delivery')); ?>">Доставка</a></li>
             <li><a href="<?php echo esc_url(home_url('/about')); ?>">О нас</a></li>
+            <div class="location-mobile-info">
+                <div class="align-center flex">
+                    <img class="image__location"
+                         src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/location.png'); ?>"
+                         alt="location">
+                    <p>
+                        <a href="https://yandex.by/maps/-/CHQv4JJD" target="_blank" rel="noopener noreferrer">
+                            Самовывоз: ул. Рабочая, 22
+                        </a>
+                    </p>
+                </div>
+                <div class="align-center flex">
+                    <img class="image__phone"
+                         src="<?php echo esc_url(get_template_directory_uri() . '/assets/img/phone.png'); ?>" alt="phone">
+                    <p>
+                        <a href="tel:+375447127117" target="_blank" rel="noopener noreferrer">
+                            +375 44 712-71-17
+                        </a>
+                    </p>
+                </div>
+            </div>
 <!--            <li><a href="--><?php //echo esc_url(home_url('/promo')); ?><!--">Акции</a></li>-->
 <!--           <li>-->
 <!---->
